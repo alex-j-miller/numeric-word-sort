0bb6d16320fa1f63fc6f0bed9a5ce06dd17a30ae
Alex Miller <alexiscool2003@sbcglobal.net>
2025-08-22 17:06:30 -0400

    started on the front end

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '@fontsource/inter';
+import InputCard from './components/InputCard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <InputCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
